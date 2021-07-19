5d1edc7b975f17355d21fc612b99748644ee483b
Bart <bartek@wroconsult.pl>
2021-07-19 15:06:44 +0200

    added project website direct links to portfolio details

--- src/utils/data/staticProjectDetailsData.jsx
+++ src/utils/data/staticProjectDetailsData.jsx
@@ -7,6 +7,7 @@ export const StaticProjectDetailsData = {
     firstHeader: '01 - Intro',
     secondHeader: '02 - Typography',
     thirdHeader: '03 - Colors',
+    fourthHeader: '04 - Website Project',
     fontPresentA: 'Aa',
     fontPresentB: <p>Aa Bb CC Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz<br />1 2 3 4 5 6 7 8 9 0 ! @ # $ % ^ & * ( )<br />Semi Bold Regular</p>,
 }
